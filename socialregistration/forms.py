6cfffdbbaecd669bb061cb9ebed3ad851729bc1c
james <james@james-desktop.(none)>
2011-02-05 01:40:28 -0500

    Added the grades user profile fields to the completed social profile form

--- socialregistration/forms.py
+++ socialregistration/forms.py
@@ -2,11 +2,14 @@ from django import forms
 from django.utils.translation import gettext as _
 
 from django.contrib.auth.models import User
+from prooffd.accounts.models import UserProfile
+from prooffd.documents.models import GRADES
 
 class UserForm(forms.Form):
 
     username = forms.RegexField(r'^\w+$', max_length=32)
     email = forms.EmailField(required=False)
+    grade = forms.ChoiceField(choices=GRADES, help_text="This will help us appropriately match your documents")
 
     def __init__(self, user, profile, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
@@ -31,6 +34,10 @@ class UserForm(forms.Form):
         if not self.user.email:
             self.user.email = self.cleaned_data.get('email')
         self.user.save()
+        user_profile = UserProfile()
+        user_profile.user = self.user
+        user_profile.grade = self.cleaned_data.get('grade')
+        user_profile.save()
         self.profile.user = self.user
         self.profile.save()
         return self.user
